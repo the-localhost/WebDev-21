7a5d094a53fa5d33304b328d9629170efc2b1ef1
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-09 23:24:32 +0530

    variable declaration

--- JavaScript/jsBasics.js
+++ JavaScript/jsBasics.js
@@ -2,5 +2,10 @@
 
 /* This is a multi-line comment.
     We can write multiple lines of comments here 
+    I'll be using comments in rest of this file
+    to explain what I'v written
 */
+
+// to declare a variable, use 'var' keyword
+var myVariable;
 
