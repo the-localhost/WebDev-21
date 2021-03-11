217662d77d3d497102569f595a02d18679c392a4
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-11 19:36:59 +0530

    learning about global scope variables

--- JavaScript/jsBasics.js
+++ JavaScript/jsBasics.js
@@ -230,3 +230,23 @@ function functionWithArgs(param1, param2) {
 }
 functionWithArgs("Hello", "World!");
 functionWithArgs("You're", "welcome.");
+
+/** GLOBAL SCOPE VARIABLES
+ * Global variable: They are visible everywhere in the JS code.
+ * > variables defined outside of a function block
+ * > variables used without the var keyword
+ */
+var globalVariable = 10;
+function fun1() {
+  anotherGlobal = 5;
+}
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
