31d194dbaaa7e968edb3c4d17593df3ce9242c66
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-12 23:47:40 +0530

    logical order of else if statwmwnts

--- JavaScript/jsBasics.js
+++ JavaScript/jsBasics.js
@@ -459,3 +459,15 @@ function testElseIf(val) {
     }
 }
 testElseIf(7);
+
+// logical order of else if statwmwnts is important; if changed, can change output
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+}
+orderMyLogic(7);
