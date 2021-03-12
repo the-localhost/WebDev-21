bdcd89dffd32d7f1513969ea44689ba32ce2f040
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-13 00:00:10 +0530

    GOLF CODE using nested if else statements

--- JavaScript/jsBasics.js
+++ JavaScript/jsBasics.js
@@ -483,3 +483,24 @@ if (condition1) {
 } else {
     statementN
 }
+
+// GOLF CODE using nested if else statements
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  if (strokes==1) {
+    return names[0];
+  } else if (strokes<=par-2) {
+    return names[1];
+  } else if (strokes==par-1) {
+    return names[2];
+  } else if (strokes==par) {
+    return names[3];
+  } else if (strokes==par+1) {
+    return names[4];
+  } else if (strokes==par+2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+}
+golfScore(5, 4);
