97a02ac45bb1e04f6e015fca7153ffd884222047
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-16 23:43:01 +0530

    "use strict" to catch coding mistakes

--- JavaScript/2. ES6/es6.js
+++ JavaScript/2. ES6/es6.js
@@ -1,3 +1,6 @@
 // let : a variable with the same name can only be declared once
 // similar to const (c++) and final (java)
 let myDog = "Camper";
+
+// "use strict"; enables Strict Mode, which catches common coding mistakes and "unsafe" actions
+"use strict";
