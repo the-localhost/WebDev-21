e9e211f02851d75455ae495c40b826b64f40da3d
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-17 21:13:18 +0530

    scope of variables declared with let

--- JavaScript/2. ES6/es6.js
+++ JavaScript/2. ES6/es6.js
@@ -4,3 +4,30 @@ let myDog = "Camper";
 
 // "use strict"; enables Strict Mode, which catches common coding mistakes and "unsafe" actions
 "use strict";
+
+/* a variable declared with the let keyword inside a block, statement, or expression
+    has it's scope limited to that block, statement, or expression. Eg:
+    function abc(){
+        for(var i=0; i<10; i++){
+            ...
+        }
+        console.log(i);
+    }
+    The above code is equivalent to...
+    function abc(){
+        var i;
+        for(i=0; i<10; i++){
+            ...
+        }
+        console.log(i);
+    }
+    Here i, declared with var, has scope even after the loop and prints 10 in console.
+    On the other hand, if i is declared with let, it's scope is only inside the loop:
+    function abc(){
+        for(let i=0; i<10; i++){
+            ...
+        }
+        console.log(i);
+    }
+    This code will throw ReferenceError
+*/
