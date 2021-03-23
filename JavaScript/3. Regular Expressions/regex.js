77ddeca34b5991c0478db2f7fd2180c98ba9020a
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-23 23:59:03 +0530

    matching literal strings

--- JavaScript/3. Regular Expressions/regex.js
+++ JavaScript/3. Regular Expressions/regex.js
@@ -2,9 +2,16 @@
  *  > Regular expressions are used in programming languages to match parts of strings
  *  > we create patterns to help you do that matching
  *  > quote marks are not required within the regular expression
- */
+*/
 
 // .test() method to test a regex
 let myString = "Hello, World!";
 let myRegex = /Hello/;
-let result = myRegex.test(myString);
+let result = myRegex.test(myString);        // true
+
+// matching literal strings
+let firstString = "Hi, Kevin";
+let secondString = "Hi, kevin";
+let regex = /Kevin/;
+let firstResult = regex.test(firstString);      // true
+let secondResult = regex.test(secondString);    // false
