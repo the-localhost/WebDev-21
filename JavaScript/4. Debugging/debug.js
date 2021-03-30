ceb5c7131b73c375f651d1ac492d9133db259a05
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-30 16:40:59 +0530

    using console.log() to check variable value

--- JavaScript/4. Debugging/debug.js
+++ JavaScript/4. Debugging/debug.js
@@ -2,4 +2,14 @@
  *  > going through the code, finding issues and fixing them
  *  > syntax errors; runtime errors; logical errors
  *  > using JS console 
- */
+*/
+
+// using JS console to check the value of a variable
+// using console.log() at strategic points to show intermediate values
+let a = 5;
+let b = 1;
+a++;
+console.log(a);
+let sumAB = a + b;
+console.log(sumAB);
+
