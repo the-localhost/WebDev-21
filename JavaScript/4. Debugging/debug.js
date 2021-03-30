491baf71b2c03286c116f3c236e410619a58ab56
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-30 17:09:12 +0530

    unclosed parentheses, brackets, braces or quotes

--- JavaScript/4. Debugging/debug.js
+++ JavaScript/4. Debugging/debug.js
@@ -21,3 +21,17 @@ console.log(seven + three);
 console.log(typeof seven);
 console.log(typeof three);
 
+/** catching misspelled variable and function names
+ *  > case-sensitive names can be misspelled especially when typing fast
+ *  > console tries to find a non-existing object and complains in form of ReferenceError
+*/ 
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;      // misspelled variables
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+/** catching unclosed parentheses, brackets, braces or quotes
+ *  > best practice:
+ *      immediately include closing match, move the cursor back and continue coding
+ *  > console can show different errors based on logics; generally include SyntaxError
+*/
