9bf66026d79f21e92eebacb118dbeeb902a40662
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-31 16:46:01 +0530

    adding items using splice()

--- JavaScript/5. Basic Data Structures/basicds.js
+++ JavaScript/5. Basic Data Structures/basicds.js
@@ -68,3 +68,11 @@ const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.splice(1, 4);
 console.log(arr);   // [2, 5, 2, 1]
 
+/** adding items using splice()
+ *  > 3rd parameter : comprises of one or more arguments to be inserted at same index as 1st param
+*/
+const numbers = [10, 11, 12, 12, 15];
+const startIndex = 3;
+const amountToDelete = 1;
+numbers.splice(startIndex, amountToDelete, 13, 14);
+console.log(numbers);       // [10, 11, 12, 13, 14, 15]
