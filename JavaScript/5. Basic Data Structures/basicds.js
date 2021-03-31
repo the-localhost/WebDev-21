5de18bdaf025256bb4f90437ff580cec86f9f03a
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-31 11:44:14 +0530

    multi-dimensional arrays

--- JavaScript/5. Basic Data Structures/basicds.js
+++ JavaScript/5. Basic Data Structures/basicds.js
@@ -3,6 +3,30 @@
 */
 
 // using an array to store a collection of data
-// one-dimensional array
+// one-dimensional array: can contain boolean/string/numbers & other valid JS datatypes
 let simpleArray = ['one', 2, 'three', true, false, undefined, null];
 console.log(simpleArray.length);        // 7
+
+// multi-dimensional array: array containing other arrays/objects
+let complexArray = [
+    [
+      {
+        one: 1,
+        two: 2
+      },
+      {
+        three: 3,
+        four: 4
+      }
+    ],
+    [
+      {
+        a: "a",
+        b: "b"
+      },
+      {
+        c: "c",
+        d: "d"
+      }
+    ]
+];
