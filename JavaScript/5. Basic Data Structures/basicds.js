48a0d586063342ccfc4b16fbdfde8b686bd9b4aa
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-31 22:45:29 +0530

    accessing property names with bracket notation

--- JavaScript/5. Basic Data Structures/basicds.js
+++ JavaScript/5. Basic Data Structures/basicds.js
@@ -175,3 +175,19 @@ let userActivity = {
 };
 userActivity.data.online = 45;
 console.log(userActivity);
+
+/** accessing property names with bracket notation
+ *  > when object properties are not known before runtime or we need to access them in a more dynamic way
+*/
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+};
+function checkInventory(scannedItem) {      // scannedItem may not be known before runtime
+return foods[scannedItem];
+}
+console.log(checkInventory("apples"));      // 25
