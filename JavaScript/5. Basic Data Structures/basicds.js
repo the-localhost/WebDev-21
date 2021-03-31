b4039fca259f0e4beae40a4fb3581faf22890853
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-31 23:46:38 +0530

    modifying an array stored in an object

--- JavaScript/5. Basic Data Structures/basicds.js
+++ JavaScript/5. Basic Data Structures/basicds.js
@@ -293,3 +293,29 @@ function getArrayOfUsers(obj) {
     return Object.keys(obj);
 }
 console.log(getArrayOfUsers(users));
+
+// modifying an array stored in an object
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+};  
+function addFriend(userObj, friend) {
+    userObj.data.friends.push(friend);
+    return userObj.data.friends;
+}
+console.log(addFriend(user, 'Pete'));
