96ff298a43032889661c6d820c8b9147ebbe4657
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-03-31 11:41:23 +0530

    one dimensional array

---
+++ JavaScript/5. Basic Data Structures/basicds.js
@@ -0,0 +1,8 @@
+/** Data Structures
+ *  > multiple ways to store and access data
+*/
+
+// using an array to store a collection of data
+// one-dimensional array
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);        // 7
