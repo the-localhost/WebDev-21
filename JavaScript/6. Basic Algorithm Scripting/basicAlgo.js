6b97f875c2af1e1ad2bc78568fb807c00b54ec65
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-01 11:26:46 +0530

    return largest numbers in arrays

--- JavaScript/6. Basic Algorithm Scripting/basicAlgo.js
+++ JavaScript/6. Basic Algorithm Scripting/basicAlgo.js
@@ -38,3 +38,16 @@ function findLongestWordLength(str) {
 }
 console.log(findLongestWordLength("May the force be with you"));        // 5
 
+// return largest numbers in arrays
+function largestOfFour(arr) {
+    let largestArr = [];
+    for(let ar in arr){
+      let largestI = 0;
+      for(let i in arr[ar]){
+        if(arr[ar][i]>arr[ar][largestI]) largestI = i;
+      }
+      largestArr.push(arr[ar][largestI]);
+    }
+    return largestArr;
+}
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);        // [5, 27, 39, 1001]
