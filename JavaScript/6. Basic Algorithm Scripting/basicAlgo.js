f900ed786d7baf1ae9551d484ea1f85e6ad1a7fe
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-01 10:50:39 +0530

    converting Celsius to Fahrenheit

---
+++ JavaScript/6. Basic Algorithm Scripting/basicAlgo.js
@@ -0,0 +1,10 @@
+/** Algorithm
+ * An algorithm is a series of step-by-step instructions that describe how to do something.
+*/
+
+// converting Celsius to Fahrenheit
+function convertToF(celsius) {
+    let fahrenheit = 9*celsius/5+32;
+    return fahrenheit;
+}
+convertToF(30);
