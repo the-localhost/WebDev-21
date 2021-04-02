e5c577918ed640ece23ee8770ee288f17e6719cf
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-02 10:12:50 +0530

    creating a javascript object

--- JavaScript/7. Object Oriented Programming/oop.js
+++ JavaScript/7. Object Oriented Programming/oop.js
@@ -1,3 +1,13 @@
 /** Object Oriented Programmming
  *  > OOP is a paradigm where we try to mimic real world objects for software development
 */
+
+/** OBJECT
+ *  > tangible things people can observe and interact with
+ *  > properties: quality of object; eg: bird has wings
+ *  > methods: behaviour of object; eg: bird sings
+*/
+let dog = {
+    name : "tommy",
+    numLegs : 4
+};
