d5c82d86254f6d2c9225262fcf631d36f8723591
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-02 18:06:03 +0530

    adding methods after inheritance

--- JavaScript/7. Object Oriented Programming/oop.js
+++ JavaScript/7. Object Oriented Programming/oop.js
@@ -289,3 +289,12 @@ let duck = new Bird();
 let beagle = new Dog();
 console.log(duck.constructor);          // [Function: Bird]
 console.log(beagle.constructor);        // [Function: Dog]
+
+// adding methods after inheritance
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = () => console.log("Woof!");
+let beagle = new Dog();
