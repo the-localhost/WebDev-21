45eb9e2b61874eab20c193236e3dbad90ebf3059
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-03 17:12:09 +0530

    learn about functional programming

--- JavaScript/8. Functional Programming/funcProg.js
+++ JavaScript/8. Functional Programming/funcProg.js
@@ -2,3 +2,18 @@
  *  > approach to develop software in which code is organised into smaller basic functions
 */
 
+/** learn about functional programming
+ *  > INPUT -> PROCESS -> OUTPUT
+ *  > isolated pure functions with limited side effects
+*/
+const prepareTea = () => 'greenTea';
+const getTea = (numOfCups) => {
+  const teaCups = [];
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+const tea4TeamFCC = getTea(40);     // 40 cups of tea
+
