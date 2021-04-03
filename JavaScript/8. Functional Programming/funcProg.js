9e2dda2eae9be377600fcdb6c2d7680132e32fbe
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-03 17:21:18 +0530

    functional programming terminology

--- JavaScript/8. Functional Programming/funcProg.js
+++ JavaScript/8. Functional Programming/funcProg.js
@@ -17,3 +17,22 @@ const getTea = (numOfCups) => {
 };
 const tea4TeamFCC = getTea(40);     // 40 cups of tea
 
+/** functional programming terminology
+ *  > Callbacks: functions that are passed into another function to decide the invocation of that function
+ *  > First class fns: that can be assigned to a variable, passed into another fn, or returned from another fn just like any other normal value
+ *      Note: In JS, all fns are first class fns.
+ *  > Higher order fns:  that take a fn as an argument, or return a fn as a return value
+ *  > Lambda: fns which gets passed in or returned by other fns
+*/
+const prepareGreenTea = () => 'greenTea';
+const prepareBlackTea = () => 'blackTea';
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
