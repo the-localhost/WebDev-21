483d3b83e09a3cafd89ae2d99a81a18ff88f78a0
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-06 15:15:54 +0530

    sum all numbers in a range

--- JavaScript/9. Intermediate Algorithm Scripting/mediumAlgo.js/mediumAlgo.js
+++ JavaScript/9. Intermediate Algorithm Scripting/mediumAlgo.js/mediumAlgo.js
@@ -1,1 +1,10 @@
 /** INTEREDIATE ALGORITHM SCRIPTING */
+
+// sum all numbers in a range
+function sumAll(arr) {
+    let m = arr[0]>arr[1]?arr[0]:arr[1];
+    let n = (m==arr[0])?arr[1]:arr[0];
+    return Math.abs(((m*(m+1)) - (n*(n-1)))/2);         // sum till bigger no (included) - sum till smaller no (excluded)
+}
+sumAll([1, 4]);
+// another method: sort array, add from arr[0] to arr[1]
