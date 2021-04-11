2f96412ca14253f564cf7d86e276d7e5f7810c8a
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-11 14:45:19 +0530

    map the debris

--- JavaScript/9. Intermediate Algorithm Scripting/mediumAlgo.js/mediumAlgo.js
+++ JavaScript/9. Intermediate Algorithm Scripting/mediumAlgo.js/mediumAlgo.js
@@ -336,3 +336,11 @@ var Person = function(firstAndLast) {
 var bob = new Person('Bob Ross');
 bob.setFirstName("Haskell")
 console.log(bob.getFullName());       // Haskell Ross
+
+// map the debris
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr.map(a => {return {name: a.name, orbitalPeriod: Math.round(2*Math.PI*(Math.sqrt(Math.pow(earthRadius+a.avgAlt, 3)/GM)))};});
+}
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);       // 86400 
