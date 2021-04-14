a0447d2af1c4bb115cb95099c8c9a2cda8112eaf
Gagan-IIT <gagan.agarwal.met17@itbhu.ac.in>
2021-04-14 17:59:55 +0530

    roman numeral converter (upto 3999)

--- JavaScript/FCC Projects/projects.js
+++ JavaScript/FCC Projects/projects.js
@@ -10,3 +10,34 @@ function palindrome(str) {
     return true;
 } 
 palindrome("_eye");         // true
+
+// roman numeral converter (upto 3999)
+function convertToRoman(num) {
+    let roman = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+    let ar = [];
+    ar.push(num/1000);
+    num%=1000;
+    ar.push(num/100);
+    num%=100;
+    ar.push(num/10);
+    num%=10;
+    ar.push(num);
+    let str = '';
+    for(let i=0; i<4; i++){
+      ar[i] = Math.floor(ar[i]);
+      if(ar[i]==0) continue;
+      if(ar[i]==4){
+        str+=roman[2*i]+roman[2*i-1];
+      }else if (ar[i]>=5){
+          str+=roman[2*i-1];
+          ar[i]-=5;
+          i--;
+      }else{
+        for(let j=0; j<ar[i]; j++)
+          str+=roman[2*i];
+      }
+    }
+   console.log(ar);
+   return str;
+}
+console.log(convertToRoman(2));
